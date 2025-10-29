a32dd0d83cf6e4cdfbea4088f4bd73bf09a4eab5
Laurent Demailly <ldemailly@gmail.com>
2025-10-29 14:58:51 -0700

    working version

--- newrepo.sh
+++ newrepo.sh
@@ -26,10 +26,9 @@ echo "Creating repository '$REPO_NAME' with description '$REPO_DESC'"
 set -x -e
 
 # Create the new repository using GitHub CLI using our template repository
-gh repo create "$REPO_NAME" --description "$REPO_DESC" --template "fortio/template" --public
+FULL_REPO_NAME="fortio/$REPO_NAME"
+gh repo create "$FULL_REPO_NAME" --description "$REPO_DESC" --template "fortio/template" --public --clone
 
-# Clone the newly created repository to the local machine
-gh repo clone "$REPO_NAME"
 cd "$REPO_NAME"
 
 # Need also on the website:
@@ -49,5 +48,70 @@ go run .
 git add .
 git commit -m "Initial commit for $REPO_NAME"
 git push origin main
+
+# Create repository ruleset for main branch protection
+gh api -X POST repos/$FULL_REPO_NAME/rulesets \
+  -H "Accept: application/vnd.github+json" \
+  --input - <<EOF
+{
+  "name": "Main branch protection",
+  "target": "branch",
+  "enforcement": "active",
+  "bypass_actors": [],
+  "conditions": {
+    "ref_name": {
+      "include": ["refs/heads/main"],
+      "exclude": []
+    }
+  },
+  "rules": [
+    {
+      "type": "pull_request",
+      "parameters": {
+        "dismiss_stale_reviews_on_push": false,
+        "require_code_owner_review": false,
+        "require_last_push_approval": false,
+        "required_approving_review_count": 0,
+        "required_review_thread_resolution": false,
+        "allowed_merge_methods": ["squash"]
+      }
+    },
+    {
+      "type": "required_linear_history"
+    },
+    {
+      "type": "deletion"
+    },
+    {
+      "type": "non_fast_forward"
+    },
+    {
+      "type": "code_scanning",
+      "parameters": {
+        "code_scanning_tools": [
+          {
+            "tool": "copilot_code_review",
+            "security_alerts_threshold": "none",
+            "alerts_threshold": "none"
+          }
+        ]
+      }
+    }
+  ]
+}
+EOF
+
+# Allow squash only
+gh api -X PATCH "repos/$FULL_REPO_NAME" \
+  -F allow_merge_commit=false \
+  -F allow_rebase_merge=false \
+  -F allow_squash_merge=true
+
+gh repo edit "$FULL_REPO_NAME" --allow-update-branch=true
+
+# Disable features that aren't needed
+gh api -X PATCH "repos/$FULL_REPO_NAME" \
+  -F has_wiki=false \
+  -F has_projects=false
 
 echo "Repository $REPO_NAME created and initialized successfully."
